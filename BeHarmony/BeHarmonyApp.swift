b6ecd6fc346c2a1614d68d9ad92917fe38138fbb
gauana <gauana3@gmail.com>
2024-03-21 21:36:43 -0700

    Initial Commit

---
+++ BeHarmony/BeHarmonyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeHarmonyApp.swift
+//  BeHarmony
+//
+//  Created by Gautam Anand on 3/21/24.
+//
+
+import SwiftUI
+
+@main
+struct BeHarmonyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BeHarmony/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BeHarmony
+//
+//  Created by Gautam Anand on 3/21/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
