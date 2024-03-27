71f47bb0dfbf992c95786420a0cc159f7bff6eaf
gauana <gauana3@gmail.com>
2024-03-27 15:03:15 -0700

    Added NavBar and fetching image functionality

---
+++ BeHarmony/Core/NavBarView.swift
@@ -0,0 +1,18 @@
+//
+//  NavBarView.swift
+//  BeHarmony
+//
+//  Created by Gautam Anand on 3/27/24.
+//
+
+import SwiftUI
+
+struct NavBarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NavBarView()
+}
